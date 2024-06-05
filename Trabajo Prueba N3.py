6ab2cb35606fa50ad0b4fec9db3bf11a499fa511
mvh11 <maxvsh18@gmail.com>
2024-06-04 20:35:56 -0400

    Update Trabajo Prueba N3.py

--- Trabajo Prueba N3.py
+++ Trabajo Prueba N3.py
@@ -3,15 +3,60 @@ print("===Don Pepe===")
 print("¡Bienvenido al comercio Don Pepe!")
 
 #MENU DE REGISTRO
-def menu_registro():
-    print("Validación de usuario.")
-    print("1. Registrarse")
-    print("2. Ingresar cuenta existente")
-    resp=int(input("Ingrese su opción (1,2) ->"))
-    #Parche
-    while resp!=1 or resp!=2:
+print("Validación de usuario.")
+print("1. Registrarse")
+print("2. Ingresar cuenta existente")
+resp=int(input("Ingrese su opción (1,2) ->"))
+#Parche
+while resp!=1 and resp!=2:
+    print("¡Ingrese opción válida!")
+    resp=int(input("Ingrese su opción (1,2) ->"))    
+#MENU PRINCIPAL (STOCK, COMPRAR, CAJA VECINA, SALIR)
+def menu_principal():
+    print("~~Menu~~")
+    print("1. Ver stock a disponibilidad")
+    print("2. Realizar la compra")
+    print("3. Caja vecina (Depositar o girar dinero.)")
+    print("4. Salir")
+    resp2=int(input("Ingrese su opción (1,2,3,4) ->"))
+    #Parche2
+    while resp2!=1 and resp2!=2 and resp2!=3 and resp2!=4:
         print("¡Ingrese opción válida!")
-        resp=int(input("Ingrese su opción (1,2) ->"))
-    return True
-menu_registro()        
-fd
+        resp2=int(input("Ingrese su opción (1,2,3,4) ->"))
+    return resp2
+resp2=menu_principal()
+#IFS
+if resp2==1:
+    print("===STOCK===")
+    print("""1. Leche
+Codigo = QWERT1
+Cantidad disponible = 5
+Precio unitario = $1.100""")
+    print("""2. Pan
+Codigo = ASDFG2
+Cantidad disponible = 10
+Precio unitario = $800""")
+    print("""3. Paquete de fideos
+Codigo = ZXCVB3
+Cantidad disponible = 6
+Precio unitario = $2.500""")
+    print("""4. Enérgetica
+Codigo = LKJHG8
+Cantidad disponible = 12
+Precio unitario = $1.800""")
+    print("""5. Agua
+Codigo = XEDCR3
+Cantidad disponible = 7
+Precio unitario = $1.000""")
+    print("""6. Cigarros(Pallwo de 20 Unidades)
+Codigo = LOPIU1
+Cantidad disponible = 12
+Precio unitario = $3.700""")
+    print("""7. Aceite maravilla 900mL
+Codigo = QAZWS2
+Cantidad disponible = 5
+Precio unitario = $2.250""")
+    print("""8. Bebida cola coca 1500mL
+Codigo = LKMSW9
+Cantidad disponible = 5
+Precio unitario = $1.850""")
